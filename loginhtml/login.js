61a6614ba51b0b37989d46b0d0e4567ee2da7f9e
riya-pandey <60851737+riya-pandey@users.noreply.github.com>
2020-06-28 13:59:24 +0545

    Add files via upload

---
+++ loginhtml/login.js
@@ -0,0 +1,24 @@
+var modal = document.getElementById('log');
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+var modal = document.getElementById('signC');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+var modal = document.getElementById('create');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
