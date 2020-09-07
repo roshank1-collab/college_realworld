82c1b6e5950ef32561042e82e284aa6ab415c3af
bhandarishankar <57697293+bhandarishankar@users.noreply.github.com>
2020-09-07 17:50:27 +0545

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,11 @@ from django.urls import reverse
 
 
 class Post(models.Model):
-    
+    content = models.TextField(max_length=1000)
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes= models.IntegerField(default=0)
+    dislikes= models.IntegerField(default=0)
 
     def __str__(self):
         return self.content[:5]
